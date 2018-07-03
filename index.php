c8d7988d8e895dface2f5a7aa0e7cfe597ab176c
cliterato <chaliterato@gmail.com>
2018-07-03 21:07:52 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Discovery Session</title>
+	<meta class="metaTagTop" name="description" content="Venkata Discovery Session">
+	<meta class="metaTagTop" name="keywords" content="">
+	<meta class="metaTagTop" name="author" content="">
+	<meta class="metaTagTop" property="og:image" content="" id="social-image">
+	<meta property="og:title" content="Discovery Session">
+	<meta property="og:description" content="Venkata Discovery Session">
+	<meta property="og:url" content="https://jupitermint.clickfunnels.com/sales-page21562547">
+	<meta property="og:type" content="website">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link href="/css/common.css" rel="stylesheet">
+    <link href="/css/header.css" rel="stylesheet" >
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/owww.css" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+	<script src="/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</head>
+<body>
+  	<?php include ('templates/header.html'); ?>
+ 	<?php include ('templates/body.html'); ?>
+ 	<?php include ('templates/footer.html'); ?>
+</body>
+</html>
